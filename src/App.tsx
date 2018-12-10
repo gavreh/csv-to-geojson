5015eb7e721ae248e5a2cabdea2cf72af20d9544
Gavin Rehkemper <grehkemper@esri.com>
2018-12-10 14:47:42 -0600

    leaflet map

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
 
-class App extends Component {
+class App extends Component<{initialLatitude: number, initialLongitude: number, initialZoom: number}, { lat: number, lng: number, zoom: number }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      lat: this.props.initialLatitude,
+      lng: this.props.initialLongitude,
+      zoom: this.props.initialZoom
+    };
+  }
   render() {
+    const position: LatLngExpression = [this.state.lat, this.state.lng]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        />
+      </Map>
+    )
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialLatitude={38.795194} initialLongitude={-89.131546} initialZoom={5} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
