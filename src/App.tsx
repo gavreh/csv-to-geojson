02db23ad76793b596070baa39460d862f9cce747
Gavin Rehkemper <grehkemper@esri.com>
2018-12-10 15:48:44 -0600

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,55 @@ import './App.css';
 import 'leaflet/dist/leaflet.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import { LatLngExpression } from 'leaflet';
+import { Container, Row, Col } from 'reactstrap';
+import CsvInput from './CsvInput';
 
-class App extends Component<{initialLatitude: number, initialLongitude: number, initialZoom: number}, { lat: number, lng: number, zoom: number }> {
+class App extends Component<{initialLatitude: number, initialLongitude: number, initialZoom: number}, { lat: number, lng: number, zoom: number, csvText: string }> {
   constructor(props: any) {
     super(props);
     this.state = {
       lat: this.props.initialLatitude,
       lng: this.props.initialLongitude,
-      zoom: this.props.initialZoom
+      zoom: this.props.initialZoom,
+      csvText: ''
     };
   }
+
+  handleClick(text: string) {
+    console.log('todo - handleClick', text);
+    this.setState({
+      csvText: text
+    });
+  }
+
   render() {
     const position: LatLngExpression = [this.state.lat, this.state.lng]
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
-          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
-        />
-      </Map>
+      <div>
+        <header>
+          <h1>CSV to GeoJSON</h1>
+          <div className="instructions">
+            <p>
+              Copy in a CSV. First line is the header, and must have a <span title="'lat', 'Lat', 'LAT', 'latitude', 'Latitude', 'LATITUDE'">latitude</span> and <span title="'lng', 'Lng', 'LNG', 'lon', 'Lon', 'LON', 'long', 'Long', 'LONG', 'longitude', 'Longitude', 'LONGITUDE'">longitude</span>.
+            </p>
+          </div>
+        </header>
+        <Container fluid>
+          <Row>
+            <Col>
+              <CsvInput onClick={(text: string) => this.handleClick(text)}></CsvInput>
+            </Col>
+            <Col>
+              <Map center={position} zoom={this.state.zoom}>
+                <TileLayer
+                  attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
+                  url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+                />
+              </Map>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     )
   }
 }
---
+++ src/CsvInput.tsx
@@ -0,0 +1,42 @@
+import React, { Component, ChangeEvent } from 'react';
+import { Button, Container, Row, Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import './CsvInput.css';
+
+class CsvInput extends Component<{onClick: Function}, {value: string}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleClick(): void {
+    console.log('test', this.state.value);
+    this.props.onClick(this.state.value);
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>): void {
+    this.setState({value: event.target.value});
+  }
+  
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <FormGroup>
+                <Input type="textarea" name="inputCsv" placeholder="Copy CSV here." rows="10" value={this.state.value} onChange={(e: ChangeEvent<HTMLInputElement>) => {this.handleChange(e);}} />
+              </FormGroup>
+              <FormGroup className="float-right">
+                <Button color="primary" onClick={() => {this.handleClick();}}>Convert</Button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default CsvInput;
